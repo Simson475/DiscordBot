6c451a840c605a591d194a05023a0a183def872a
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2020-12-17 12:09:01 +0100

    dg

---
+++ DiscordBot/Modules/RussianRouletteModule.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    [Group("Russian Roulette")]
+    [Summary("A game of russian roulette!")]
+    public class RussianRouletteModule : ModuleBase<SocketCommandContext>
+    {
+        public class StartModule : ModuleBase<SocketCommandContext>
+        {
+            [Command("Start")]
+            [Summary("Starts the game")]
+            public async Task StartCommand()
+            {
+
+                await ReplyAsync($"Roulette just started");
+            }
+
+        }
+
+    }
+}
