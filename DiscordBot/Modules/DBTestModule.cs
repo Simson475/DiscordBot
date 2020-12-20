c0a17efdb30af5c6e3925033af035d7636e1e16d
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2020-12-20 18:32:53 +0100

    updated so we can handle gifs

---
+++ DiscordBot/Modules/DBTestModule.cs
@@ -0,0 +1,25 @@
+﻿//using Discord.Commands;
+//using MongoDB.Bson;
+//using MongoDB.Driver;
+//using System;
+//using System.Threading.Tasks;
+
+//namespace DiscordBot
+//{
+//    public class DBTestModule : ModuleBase<SocketCommandContext>
+//    {
+//        [Command("DB")]
+//        [Summary("Test Command")]
+//        public async Task DBCommand()
+//        {
+//            string password = Environment.GetEnvironmentVariable("password");
+//            MongoClient client = new MongoClient($"mongodb+srv://dbUser:{password}@botdb.soulu.mongodb.net/<dbname>?retryWrites=true&w=majority");
+//            IMongoDatabase database = client.GetDatabase("DiscordBot");
+//            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("Test");
+
+//            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("student_id", 10000 );
+//            BsonDocument test = collection.Find<BsonDocument>(filter).FirstOrDefault();
+//            await Context.Channel.SendMessageAsync(test.ToString());
+//        }
+//    }
+//}
